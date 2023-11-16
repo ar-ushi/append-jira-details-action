26fdc53e6cfa842a3e237604e39a136734614ab7
Arushi <aggarwalarushi5@gmail.com>
2023-11-17 01:33:58 +0530

    setup base for api req

---
+++ src/actions.ts
@@ -0,0 +1,4 @@
+import * as core from '@actions/core';
+import * as gh from '@actions/github';
+import { Octokit } from '@octokit/rest';
+
---
+++ src/getDetails.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core';
+
+interface getDetailsInput{
+    authToken: string;
+    jiraAPIUrl: string;
+}
+
+interface Fields{
+    [key: string] : any;
+}
+
+export default async ({authToken, jiraAPIUrl} : getDetailsInput): Promise<Fields>=> {
+try {
+    core.info('fetching details...');
+    const response = await fetch(jiraAPIUrl, {
+        headers: {
+            Authorization: `Basic ${authToken}`
+        }
+    })
+    if (response.ok){
+        const {fields} = await response.json();
+        return fields;
+    } else {
+        throw new Error ('No response from Jira API');
+    }
+} catch (error) {
+    core.setFailed(error.message);
+    process.exit(1);
+}
+};
