b5adc925349376ee886a355117b7bfaca1ffba93
ar-ushi <aggarwalarushi5@gmail.com>
2024-09-12 13:20:26 +0530

    feat - clean description formatter, check fix versions and update action to run on synchronize

--- src/actions.ts
+++ src/actions.ts
@@ -4,13 +4,26 @@ import { Octokit } from '@octokit/rest';
 import retrieveDetails from './retrieve-details';
 import fetch from 'node-fetch';
 
-function cleanAndFormatDescription(description: string): string {    
+function cleanAndFormatDescription(description: string): string {
     return description
-    .replace(/{noformat}/g, '```')
-    .replace(/{noformat}/g, '```')
-    .replace(/!\S.*?!/g, '')
-    .trim(); 
+      .replace(/{noformat}/g, '```')
+      .replace(/^\{(?!noformat)[^}]+\}.*$/gm, '')
+      .replace(/!\S.*?!/g, '')
+      .replace(/^# /gm, '- ')
+      .replace(/^## /gm, '1. ') 
+      .trim();
   }
+
+function addDescriptionToBody(jiradesc: string, body: string){
+    const dividingLine = '---';
+    const newJiraBlock = `$*Jira Description** \n\n${jiradesc}\n\n${dividingLine}`;
+    const regex = new RegExp(`*Jira Description** \n\n([\\s\\S]*?)\\n${dividingLine}`, 'm');
+    if (regex.test(body)){
+        return body.replace(regex, newJiraBlock);
+    } else {
+        return `${newJiraBlock}\n\n${body}`
+    }
+}
 
 export default async function getDetailsForPr() {
  try {
@@ -19,6 +32,7 @@ export default async function getDetailsForPr() {
         summary: string;
         description: string;
         issueType: string;
+        fixVersions: Array<string>,
     }      
     const GHtoken = core.getInput('token', {required: true});
     const jiraKey = core.getInput('jiraKey', { required: true }).split(',').map(key => key.trim());
@@ -58,20 +72,11 @@ export default async function getDetailsForPr() {
         jiraKey.forEach(key => {
             commitMessages.forEach(msg => extractJiraIds(msg, key));
             extractJiraIds(pullRequestTitle, key);
+            extractJiraIds(branch_name, key)
         });
     
-    // If no Jira IDs found in the title, check the branch name
     if (jiraIds.size === 0) {
-        for (const key of jiraKey) {
-            const jiraIdMatch = branch_name.match(new RegExp(`${key}-\\d+`));
-            if (jiraIdMatch) {
-            jiraIds.add(jiraIdMatch[0]);
-            break; 
-            }
-        }
-    }
-    if (jiraIds.size === 0) {
-        throw new Error(`Could not find any Jira IDs in the PR title or branch name matching any of the Jira keys: ${jiraKey.join(', ')}`);
+        throw new Error(`Could not find any Jira IDs matching any of the Jira keys: ${jiraKey.join(', ')}`);
     }
     for (const jiraId of jiraIds) {
         const jiraAPIUrl = `${orgUrl}/rest/api/2/issue/${jiraId}`;
@@ -79,18 +84,23 @@ export default async function getDetailsForPr() {
           authToken,
           jiraAPIUrl,
     });
+        const fixVersions = fields.fixVersions.map((fv: { name: string; }) => fv.name);
         let desc = fields.summary;
         if (fields.description && fields.description.trim() !== '') {
             desc = cleanAndFormatDescription(fields.description);
         } 
-        jiraDetails.push({id: jiraId as string, summary: fields.summary, description: desc, issueType: fields.issuetype.name})
+        jiraDetails.push({id: jiraId as string, summary: fields.summary, description: desc, issueType: fields.issuetype.name, fixVersions: fixVersions})
     }
 
     const title = jiraDetails.length === 1 ?`${jiraDetails[0].id} | ${jiraDetails[0].summary}` :  jiraDetails.map(jira => jira.id).join(' & ');
     const jiraDescriptions = jiraDetails.map(jira => `${jira.id}: ${jira.description}`).join('\n\n');
     const issueTypes = jiraDetails.map(jira => jira.issueType.toLowerCase());
+    const fixVersions = jiraDetails.map(jira => jira.fixVersions).flat();
+    
+    const labelsToAdd = [...issueTypes, ...fixVersions];
 
-    const body = `**Jira Description** \n\n${jiraDescriptions}\n\n## ${bodyContent}`;
+    // const body = `**Jira Description** \n\n${jiraDescriptions}\n\n## ${bodyContent}`;
+    const body = addDescriptionToBody(jiraDescriptions, bodyContent!);
     await client.rest.pulls.update({
         owner,
         repo,
@@ -102,11 +112,9 @@ export default async function getDetailsForPr() {
         owner,
         repo,
         issue_number : pull_number,
-        labels: issueTypes
+        labels: labelsToAdd.filter(label => label)
     })
  } catch (error : any) {
     core.setFailed(`process failed with ::: ${error.message}`);
  }   
 }
-
-
